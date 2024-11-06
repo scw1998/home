ec58b8bddd48a78ec80c37f15c1d7cc62f6a2732
Chi Won Seuh <chiwonseuh@Chis-MacBook-Pro.local>
2024-11-06 14:43:20 -0600

    plz

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/scw1998/scw1998.github.io";
 
 ghpages.publish(
   pathname,
